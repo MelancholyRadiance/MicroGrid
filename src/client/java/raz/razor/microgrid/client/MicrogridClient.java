9e7a204661a5f635ac3becef1059dc53cdf17b33
MelancholyRadiance <rybloor6@outlook.com>
2025-10-23 23:57:42 +0100

    additions

---
+++ src/client/java/raz/razor/microgrid/client/MicrogridClient.java
@@ -0,0 +1,10 @@
+package raz.razor.microgrid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MicrogridClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/raz/razor/microgrid/Microgrid.java
@@ -0,0 +1,10 @@
+package raz.razor.microgrid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Microgrid implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
